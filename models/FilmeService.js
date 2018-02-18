7c12c72ed1be9c4582dd318159400967a4d7a147
Rafael Cardoso <rafaelalbuquerquecardoso@gmail.com>
2018-02-17 22:38:38 -0300

    Baseando busca e detalhes em serviço

---
+++ models/FilmeService.js
@@ -0,0 +1,47 @@
+"use strict";
+var request = require('request');
+
+class FilmeService {
+
+    pageList(urlAPI, page, callback) {
+        var urlPage = urlAPI+"&page="+page;
+        request(urlPage, function (error, response, body) {
+            if (error == null) {
+                var data = JSON.parse(body);
+                console.log("Request funcionou, dados: "+data);
+                return callback(null, data.results);
+            } else {
+                console.log("Ocorreu um erro ao buscar a página "+page+" - Error: "+error);
+                return callback(error, null);
+            }
+        });
+   }
+
+   filmeProduction(urlAPI, callback) {
+    request(urlAPI, function (error, response, body) {
+        if (error == null) {
+            var data = JSON.parse(body);
+            console.log("Request funcionou, dados: "+data);
+            return callback(null, data);
+        } else {
+            console.log("Ocorreu um erro ao buscar o fime - Error: "+error);
+            return callback(error, null);
+        }
+    });
+   }
+
+   filmeDetails(urlAPI, callback) {
+    request(urlAPI, function (error, response, body) {
+        if (error == null) {
+            var data = JSON.parse(body);
+            console.log("Request funcionou, dados: "+data);
+            return callback(null, data);
+        } else {
+            console.log("Ocorreu um erro ao buscar o fime - Error: "+error);
+            return callback(error, null);
+        }
+    });
+   }
+}
+
+module.exports = FilmeService;
--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,35 @@ var router = express.Router();
 var request = require('request');
 var FilmeModel = require("../models/FilmeModel");
 var filmeModel = new FilmeModel();
+var urlAPI = "https://api.themoviedb.org/3/discover/movie?api_key=516df799631d51e95f9abca329a46d83&language=pt-BR&sort_by=popularity.desc&with_genres=878&include_video=false";
+
+var FilmeService = require("../models/FilmeService");
+var filmeService = new FilmeService();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/filmes', function (req, res, next) {
-    filmeCtrl.showData(function(data) {
-        res.render('filmelist', { "filmelist": data });      
-    });
-});
-
-router.get('/sincronizar', function(req, res, next) {
-  var urlAPI = "https://api.themoviedb.org/3/discover/movie?api_key=516df799631d51e95f9abca329a46d83&language=pt-BR&sort_by=popularity.desc&with_genres=878&include_video=false";
-
-  request(urlAPI, function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var data = JSON.parse(body);
-        var totalPages = parseInt(data.total_pages);
-        //filmeModel.updateFilmes(urlAPI, totalPages);
+  filmeService.pageList(urlAPI, 1, function(err, result) {
+    if (!err) {
+      res.render('index', { filmelist: result, msg: null });
+    } else {
+      res.render('index', { filmelist: null, msg: "O serviço está fora do ar" });
     }
   });
-
-  res.render('index', { title: 'Express' });
 });
+
+router.get('/filme/:id', function (req, res, next) {
+  var urlFilme = "https://api.themoviedb.org/3/movie/"+ req.params.id +"?api_key=516df799631d51e95f9abca329a46d83&language=pt-BR";  
+  var urlProd = "https://api.themoviedb.org/3/movie/"+ req.params.id +"/credits?api_key=516df799631d51e95f9abca329a46d83&language=pt-BR";    
+  filmeService.filmeDetails(urlFilme, function(err, result) {
+    if (!err) {
+      filmeService.filmeProduction(urlProd, function(error, resData){
+        console.log(JSON.stringify(resData));
+        res.render('details', { filme: result, production: resData, msg: null });        
+      });
+    } else {
+      res.render('details', { filme: null, msg: "O serviço está fora do ar" });
+    }
+  });
+});
+
 
 module.exports = router;
